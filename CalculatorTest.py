c079c457cd8f7aa6aa3a9551ff5c60b989464f66
pcastellanosandes <pa.castellanos11@uniandes.edu.co>
2017-03-19 15:10:28 -0500

    Step 4: test to add multiple numbers (RED)

--- CalculatorTest.py
+++ CalculatorTest.py
@@ -18,3 +18,6 @@ class CalculatorTest(TestCase):
 
     def test_addTwoNumbers(self):
         self.assertEqual(Calculator().add("1,3"), 4, "Two numbers")
+
+    def test_addMultipleNumbers(self):
+        self.assertEqual(Calculator().add("5,2,4,1"), 12, "Multiple numbers")
