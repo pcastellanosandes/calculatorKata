6279780633fbdae725409fbc2c34e71ed6cf7e2a
pcastellanosandes <pa.castellanos11@uniandes.edu.co>
2017-03-19 14:36:06 -0500

    Step 2: test to add just one number (RED)

--- CalculatorTest.py
+++ CalculatorTest.py
@@ -8,3 +8,6 @@ __autor__ = 'Paula Castellanos'
 class CalculatorTest(TestCase):
     def test_add(self):
         self.assertEqual(Calculator().add(""), 0, "empty string")
+
+    def test_addOneNumber(self):
+        self.assertEqual(Calculator().add("1"), 1, "One number")
