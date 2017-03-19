3c9f781d5a13560285a0634d81ccac7619959e20
pcastellanosandes <pa.castellanos11@uniandes.edu.co>
2017-03-19 15:07:35 -0500

    Step 3: test adding two different numbers (GREEN)

--- Calculator.py
+++ Calculator.py
@@ -4,5 +4,7 @@ class Calculator:
     def add(self, numbers):
         if numbers == "":
             return 0
+        elif ',' in numbers:
+            return int(numbers[0]) + int(numbers[2])
         else:
             return int(numbers)
